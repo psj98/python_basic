809680129accc6635509c385e8936149369e52ba
psj98 <park98sj1@hanmail.net>
2022-06-23 22:23:39 +0900

    python 131 to 140

---
+++ 101to200/131to140.py
@@ -0,0 +1,48 @@
+# 파이썬 분기문
+
+# 131번
+# 사과
+# 귤
+# 수박
+
+# 132번
+# #####
+# #####
+# #####
+
+# 133번
+print('A')
+print('B')
+print('C')
+
+# 134번
+print('출력 : A')
+print('출력 : B')
+print('출력 : C')
+
+# 135번
+print('변환 : a')
+print('변환 : b')
+print('변환 : c')
+
+# 136번
+for i in [10, 20, 30]:
+    print(i)
+
+# 137번
+for i in [10, 20, 30]:
+    print(i)
+
+# 138번
+for i in [10, 20, 30]:
+    print(i)
+    print('-------')
+
+# 139번
+print('++++')
+for i in [10, 20, 30]:
+    print(i)
+
+# 140번
+for i in range(4):
+    print('-------')
