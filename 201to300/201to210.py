737c3527deb0b94ff673841ec3af9d7ca9a11842
psj98 <park98sj1@hanmail.net>
2022-06-26 00:41:20 +0900

    python 201 to 210

---
+++ 201to300/201to210.py
@@ -0,0 +1,52 @@
+# 파이썬 함수
+
+# 201번
+def print_coin():
+  print('비트코인')
+
+# 202번
+print_coin()
+
+# 203번
+for i in range(100):
+  print_coin()
+
+# 204번
+def print_coins():
+  for i in range(100):
+    print('비트코인')
+
+# 205번
+# 함수가 정의되기 전에 호출되어서 에러 발생
+
+# 206번
+# A
+# B
+# C
+# A
+# B
+
+# 207번
+# A
+# C
+# B
+
+# 208번
+# A
+# C
+# B
+# E
+# D
+
+# 209번
+# B
+# A
+
+# 210번
+# B
+# C
+# B
+# C
+# B
+# C
+# A
