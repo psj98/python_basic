758e21af3745b47f820c8290ec6cbd264cf9d9d8
psj98 <park98sj1@hanmail.net>
2022-06-26 02:56:20 +0900

    python 231 to 240

---
+++ 201to300/231to240.py
@@ -0,0 +1,42 @@
+# 파이썬 함수
+
+# 231번
+# ERROR
+
+# 232번
+def make_url(url):
+  return 'www.'+url+'.com'
+
+# 233번
+def make_list(li):
+  to_list = []
+  for i in li:
+    to_list.append(i)
+  return to_list
+
+# 234번
+def pickup_even(even):
+  to_even = []
+  for i in even:
+    if i % 2 == 0:
+      to_even.append(i)
+  return to_even
+
+# 235번
+def convert_int(str):
+  return int(str.replace(',', ''))
+
+# 236번
+# 22
+
+# 237번
+# 22
+
+# 238번
+# 140
+
+# 239번
+# 16
+
+# 240번
+# 28
